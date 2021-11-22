bb3846bf03a490f3ec8a58ef0f57cebc0ef47695
Blener332 <94755737+Blener332@users.noreply.github.com>
2021-11-22 23:05:38 +0100

    Add files via upload

---
+++ stronka_podrasowana/logowanie.php
@@ -0,0 +1,109 @@
+<?php
+	
+	session_start();
+	
+?>
+
+
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8">
+    <title>Nasza stronka</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <!-- BOOTSTRAP 4 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   
+  </head>
+  <body>
+	
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    
+    <div class="mx-5 order-0">
+        <a class="navbar-brand mx-auto" href="index.php">Nasza strona</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav mr-5">
+            <li class="nav-item">
+                <a class="nav-link" 
+				<?php
+				if(isset($_SESSION['zalogowany'])) 
+				{
+					echo "href='wyloguj.php' >Wyloguj";
+				} 
+				else 
+				{
+					echo "href='logowanie.php'>Zaloguj";
+				} 
+				?></a>
+            </li>
+			
+            <li class="nav-item">
+                <a class="nav-link" href="rejestracja.php">Zarejestruj</a>
+            </li>
+			<li class="nav-item">
+                <a 
+				<?php
+				if(isset($_SESSION['zalogowany'])) 
+				{
+					echo "class='nav-link'";
+				} 
+				else 
+				{
+					echo "class='nav-link disabled'";
+				} 
+				?>
+				 href="edytowanie.php">Edytuj</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+	
+	<div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-7">
+                <h3 class="mb-4 text-center">Logowanie</h3>
+                <div class="form-body bg-light p-4">
+                    <form action="zaloguj.php" method="post">
+                        <div class="row">
+                            <div class="col-lg-6 mb-3">
+                                <label for="login" class="form-label">Login*</label>
+                                <input type="text" class="form-control" id="login" name="login">
+                                
+                            </div>
+                            <div class="col-lg-6 mb-3">
+                                <label for="nazwisko" class="form-label">Hasło*</label>
+                                <input type="password" class="form-control" id="haslo" name="haslo">
+                            
+                            </div>
+                            <div class="col-lg-12">
+                                <input type="submit" class="btn btn-dark form-control" name="dodaj" value="Zaloguj">
+                            </div>
+							
+							<div class="col-lg-6 mb-3">
+							<?php
+
+								if(isset($_SESSION['blad'])){
+									echo $_SESSION['blad'];
+									unset($_SESSION['blad']);
+								}
+							?>
+							</div>
+							
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
